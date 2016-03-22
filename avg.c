c99767facd5ffe7692a5b78814a38eead474e3ff
Caroline <caroline.swinyer@gmail.com>
2016-03-21 20:37:53 -0400

    Added files via upload
    Quick little calculator, written in ~15 min

---
+++ avg.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char** argv){
+
+	char* s = (char*)malloc(sizeof(int));
+	int count = 0, sum = 0, grade = 0;
+	float total = 0;
+
+	printf("Quick averager\n------------\n\nEnter in a digit:\n");
+	scanf("%s", s);
+	
+	do {
+		
+		grade = atof(s);
+		printf("\n\tNum: %d\n", grade);
+
+		sum += grade;
+		count++;
+		
+		free(s);
+		printf("Enter next digit, or q to quit: \n");
+		scanf("%s", s);
+		
+	} while ((strcmp(s, "q")) != 0);
+	
+	//printf("\nSum: %d\tCount: %d", sum, count);
+	total = sum/(float)count;
+	printf("\n\nTotal: %0.3f\n\n", total);
+	
+	return 0;
+	
+}
